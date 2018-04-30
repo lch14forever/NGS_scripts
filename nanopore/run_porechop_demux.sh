16a7ff4ee4434362aa9f6040732b521e92ada565
Chenhao Li <lch14forever@gmail.com>
2018-04-30 12:29:15 +0800

    bug fix

--- nanopore/run_porechop_demux.sh
+++ nanopore/run_porechop_demux.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -e -o pipefail
 
 input=$1
@@ -8,7 +7,7 @@ threads=${2:-4}
 echo Running porechop with $threads theads...
 
 bs=`basename $input`
-bs=${bs%%.fastq.gz}
+bs=${bs%%.fastq*}
 dir=`dirname $input`
 demux_folder=${dir}/demux_$bs
 
@@ -18,12 +17,18 @@ $PORECHOP -i $input -b $demux_folder -t $threads -v 2
 
 echo Renaming output folders...
 
-for i in ${demux_folder}/*fastq;
+for i in ${demux_folder}/*;
 do
     bs_i=`basename $i`
-    f=${dir}/${bs%%_*}_${bs_i%%.fastq}
-    gzip $i
-    mkdir $f
-    mv $i.gz ${f}/${bs%%_*}_${bs_i}.gz
+    f=${dir}/${bs%%_*}_${bs_i%%.fastq*}
+    if [[ $i == *.fastq ]] ## results in fastq format
+    then
+	gzip $i
+	mkdir $f
+	mv $i.gz ${f}/${bs%%_*}_${bs_i}.gz
+    else                  ## results gizpped
+        mkdir $f
+        mv $i ${f}/${bs%%_*}_${bs_i}	
+    fi
 done
 rmdir $demux_folder
