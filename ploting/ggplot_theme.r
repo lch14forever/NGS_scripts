085eeb58445f2cb2507ea6a8b7a05d2e6cc92c78
Chenhao Li <lch14forever@gmail.com>
2019-06-19 11:16:27 +0800

    ggplot theme

--- ploting/ggplot_theme.r
+++ ploting/ggplot_theme.r
@@ -9,9 +9,23 @@ figtheme <- theme_bw() +
         axis.title.y=element_text(margin=margin(0,15,0,0)),axis.title.x=element_text(margin=margin(15,0,0,0)),
         plot.margin = unit(c(1,1,1,1), "cm"),
         plot.title = element_text(margin=margin(0,0,15,0), hjust=0.5))
-theme_set(figtheme)
+
+figtheme <- theme_classic() +
+  theme(text = element_text(size=23,face='bold'),panel.border  = element_rect(colour = "black",size=2),
+        axis.title.y=element_text(margin=margin(0,15,0,0)),axis.title.x=element_text(margin=margin(15,0,0,0)),
+        plot.margin = unit(c(1,1,1,1), "cm"),
+        plot.title = element_text(margin=margin(0,0,15,0), hjust=0.5))
+
+use_theme_bw <- function(){
+	theme_set(figtheme)
+	}
+use_theme_classic <- function(){
+	theme_set(figtheme_classic)
+	}
 
 gg_color_hue <- function(n) {
   hues = seq(15, 375, length=n+1)
   hcl(h=hues, l=65, c=100)[1:n]
 }
+
+sel_col <- c(pal_npg('nrc')(10), pal_rickandmorty()(12), pal_tron()(7)[4])[-c(8,9,13,18,21,22)]
