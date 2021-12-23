514624dd0da4877a40335861a16c89f91c949b93
Chenhao Li <lch14forever@gmail.com>
2021-12-23 16:24:08 -0500

    Using temporary json file for `run_job.sh`

--- run_job.sh
+++ run_job.sh
@@ -9,10 +9,16 @@ status=$?
 stderr=`cat /tmp/job.stderr`
 stdout=`cat /tmp/job.stdout`
 ## Text message
-MSG_SUCC="%23ExecutionLog%0ACommand: <b>$cmd</b>%0ASuccessful!"
-MSG_FAIL="%23ExecutionLog%0ACommand: <b>$cmd</b>%0AFailed!%0Aexit code: <b>$status</b>%0AStderr: $stderr"
+MSG_SUCC="#ExecutionLog\nCommand: <b>$cmd</b>\nSuccessful!"
+MSG_FAIL="#ExecutionLog\nCommand: <b>$cmd</b>\nFailed!%0Aexit code: <b>$status</b>\nStderr: $stderr"
+[ $status -eq 0 ] && msg=$MSG_SUCC || msg=$MSG_FAIL
+
+## JSON file
+JSON_FMT='{"chat_id":"%s","text":"%s","parse_mode":"html"}\n'
+printf "$JSON_FMT" "$PRIVATE_CHAT_ID" "$msg" > /tmp/job.json
 
 ## Send message
 [ $status -eq 0 ] && msg=$MSG_SUCC || msg=$MSG_FAIL
 curl -X POST -H 'Content-Type: application/json' \
-     "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage?chat_id=$PRIVATE_CHAT_ID&text=$msg&parse_mode=html"
+     -d @/tmp/job.json \
+     "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
